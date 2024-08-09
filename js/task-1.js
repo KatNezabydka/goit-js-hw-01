a97b27883b32270624a00cb8b6bfdcd2e9fa82e7
kateryna_kornysheva <k.kornysheva@betinvest.com>
2024-08-09 19:32:45 +0200

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,5 @@
-function makeTransaction(quantity, totalPrice) {
+function makeTransaction(quantity, pricePerDroid) {
+    let totalPrice = pricePerDroid * quantity;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
